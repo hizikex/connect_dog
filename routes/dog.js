77d625dc0e155366190e002956bb77bd7ddcea50
Isaac Ugwuanya <hizikugwuanya@gmail.com>
2024-05-30 15:08:53 +0100

    authenticate important route

--- routes/dog.js
+++ routes/dog.js
@@ -5,7 +5,7 @@ import { authenticate } from '../middleware/authentication.js'
 const Route = express.Router();
 
 Route.post('/register', authenticate, registerDog);
-Route.get('/:uniqueName', findDogByUniqueName);
-Route.get('/', findDogs);
+Route.get('/:uniqueName', authenticate, findDogByUniqueName);
+Route.get('/', authenticate, findDogs);
 
 export default Route;
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { findOwners, findUser, login, registerUser } from "../controller/user.js";
+import { authenticate } from '../middleware/authentication.js'
 
 const Route = express.Router();
 
   Route.post('/register', registerUser);
   Route.post('/login', login);
-  Route.get('/:username', findUser);
-  Route.get('/', findOwners);
+  Route.get('/:username', authenticate, findUser);
+  Route.get('/', authenticate, findOwners);
   
 export default Route;

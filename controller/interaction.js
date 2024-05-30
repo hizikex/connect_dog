cb9dddc7f818ce25f3f470f2a98c3a16a9ac3d52
Isaac Ugwuanya <hizikugwuanya@gmail.com>
2024-05-31 00:30:28 +0100

    interact with dog owners

--- controller/interaction.js
+++ controller/interaction.js
@@ -1,14 +1,16 @@
 import Interaction from "../models/interaction.js";
 import User from "../models/user.js";
 import Dog from "../models/dog.js";
+import sendEmail from "../utils/nodemailer.js";
 
 export const ownerInteraction = async (req, res) => {
   const { senderDogId, recipientDogId } = req.params;
   const senderDog = await Dog.findOne({ _id: senderDogId });
   const recipientDog = await Dog.findOne({ _id: recipientDogId });
-  const sender = await Dog.findOne({ _id: senderDogId }).populate('ownerId')._id;
-  console.log(senderDog);
-  const recipient = await Dog.findOne({ _id: recipientDogId }).populate('ownerId')._id;
+  const sender = await Dog.findOne({ _id: senderDogId }).populate('ownerId');
+  console.log(sender.ownerId.email);
+  const recipient = await Dog.findOne({ _id: recipientDogId }).populate('ownerId')
+  console.log(recipient.ownerId.email);
 
   if (!senderDog) {
     return res.status(404).json({
@@ -21,6 +23,14 @@ export const ownerInteraction = async (req, res) => {
       message: "Dog does not exist",
     });
   }
+
+  const message = `This is my whatApp number ${sender.ownerId.phoneNumber}`;
+
+  sendEmail({
+    email: recipient.ownerId.email,
+    subject: "Dog connection alert",
+    message
+  });
 
   const interaction = await Interaction.create({
     senderDogId: senderDog.id,
